73bcd5c79da644e23f4ee96ec1725131aa8d8b1b
Unai Sarasola <unaisarasola@hotmail.com>
2025-06-26 20:22:46 +0200

    Landing refactorized

--- src/mam_analyzer/phases/final_landing.py
+++ src/mam_analyzer/phases/final_landing.py
@@ -4,6 +4,7 @@ from typing import List, Optional, Tuple, Dict, Any
 from mam_analyzer.context import FlightDetectorContext
 from mam_analyzer.models.flight_events import FlightEvent
 from mam_analyzer.detector import Detector
+from mam_analyzer.utils.search import find_first_index_forward,find_first_index_forward_starting_from_idx
 from mam_analyzer.utils.units import heading_within_range
 
 class FinalLandingDetector(Detector):
@@ -21,29 +22,45 @@ class FinalLandingDetector(Detector):
         landing_end = None
 
         # Step 1: First event with LandingVSFpm
-        for idx in range(len(events) - 1, -1, -1):
-            event = events[idx]
-            if event.other_changes.get("LandingVSFpm") is not None:
-                touch_idx = idx
-                touch_heading = event.heading
-                landing_start = event.timestamp
-                break
+        def withLandingVSFpm(e: FlightEvent) -> bool:
+            return e.other_changes.get("LandingVSFpm") is not None
 
+        found_landing = find_first_index_forward(
+            events,
+            withLandingVSFpm,
+            from_time,
+            to_time
+        )
 
-        if landing_start is None:
+        if found_landing is None:
             return None  # Landing not detected
+        else:
+            touch_idx, landing_event = found_landing
+            touch_heading = landing_event.heading
+            landing_start = landing_event.timestamp
 
         # Step 2: Look for the end of the landing until heading variate from touch_idx
         # TODO: Needed limit of time or check speed?
-        for idx in range(touch_idx + 1, len(events)):
-            event = events[idx]
-            if event.heading is not None and not heading_within_range(touch_heading, event.heading):
-                landing_end = events[idx - 1].timestamp
-                break
 
-        if landing_end is None:
+        def headingOutOfRange(e: FlightEvent) -> bool:
+            return (
+                e.heading is not None 
+                and not heading_within_range(touch_heading, e.heading)
+            )
+
+        found_end_landing = find_first_index_forward_starting_from_idx(
+            events,
+            touch_idx + 1,
+            headingOutOfRange,
+            from_time,
+            to_time
+        )
+
+        if found_end_landing is None:
             # Just the last event
-            landing_end = events[-1].timestamp                    
-
+            landing_end = events[-1].timestamp
+        else:
+            first_bad_heading_idx, _ = found_end_landing
+            landing_end = events[first_bad_heading_idx - 1].timestamp                
 
         return landing_start, landing_end
