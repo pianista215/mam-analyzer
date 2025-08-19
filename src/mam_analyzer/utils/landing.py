0fc41f62646435a7315966e94f1a881c3c675c41
Unai Sarasola <unaisarasola@hotmail.com>
2025-08-19 13:35:38 +0200

    Added distance and bounces

---
+++ src/mam_analyzer/utils/landing.py
@@ -0,0 +1,7 @@
+from mam_analyzer.models.flight_events import FlightEvent
+
+def event_has_landing_vs_fpm(e: FlightEvent) -> bool:
+	return e.other_changes.get("LandingVSFpm") is not None
+
+def get_landing_vs_fpm_as_int(e: FlightEvent) -> int:
+	return int(e.other_changes.get("LandingVSFpm"))	
--- src/mam_analyzer/utils/units.py
+++ src/mam_analyzer/utils/units.py
@@ -1,4 +1,4 @@
-from math import isclose
+from math import isclose, radians, sin, cos, atan2, sqrt
 
 def heading_within_range(h1: int, h2: int, tolerance: int = 6) -> bool:
     """Returns True if the headings are within the specified ±tolerance degrees."""
@@ -7,4 +7,20 @@ def heading_within_range(h1: int, h2: int, tolerance: int = 6) -> bool:
 
 
 def coords_differ(a: float, b: float, tolerance: float = 1e-6) -> bool:
-    return not isclose(a, b, abs_tol=tolerance)    
+    return not isclose(a, b, abs_tol=tolerance)
+
+def haversine(lat1, lon1, lat2, lon2):
+    # Earth radius in meters
+    R = 6371000  
+    
+    # Coords to radians
+    phi1 = radians(lat1)
+    phi2 = radians(lat2)
+    dphi = radians(lat2 - lat1)
+    dlambda = radians(lon2 - lon1)
+    
+    # Haversine
+    a = sin(dphi/2)**2 + cos(phi1) * cos(phi2) * sin(dlambda/2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    
+    return R * c    
