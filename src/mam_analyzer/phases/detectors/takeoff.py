5315f81f63907daaf952e7e559ac1b8d81c6872e
Unai Sarasola <unaisarasola@hotmail.com>
2026-02-12 10:52:03 +0100

    Takeoff detector tuned with runways

--- src/mam_analyzer/phases/detectors/takeoff.py
+++ src/mam_analyzer/phases/detectors/takeoff.py
@@ -7,7 +7,7 @@ from mam_analyzer.utils.ground import is_on_air
 from mam_analyzer.utils.location import event_has_location
 from mam_analyzer.utils.runway import build_runway_polygon, match_runway_end, point_inside_runway
 from mam_analyzer.utils.search import find_first_index_forward,find_first_index_backward_starting_from_idx,find_first_index_forward_starting_from_idx
-from mam_analyzer.utils.units import heading_within_range
+from mam_analyzer.utils.units import haversine, heading_within_range
 
 class TakeoffDetector(Detector):
     def detect(
@@ -57,18 +57,31 @@ class TakeoffDetector(Detector):
             )
 
         if runway_match is not None:
-            rwy, _ = runway_match
+            rwy, matched_end = runway_match
             rwy_polygon = build_runway_polygon(rwy)
 
-            # Walk backward: find last event OUTSIDE the runway polygon or with heading change
+            min_distance = haversine(
+                airborne_event.latitude, airborne_event.longitude,
+                matched_end.latitude, matched_end.longitude,
+            )
             takeoff_start = events[0].timestamp
+
             for idx in range(airborne_idx - 1, -1, -1):
                 e = events[idx]
-                outside_polygon = event_has_location(e) and not point_inside_runway(e.latitude, e.longitude, rwy_polygon)
-                heading_changed = e.heading is not None and not heading_within_range(e.heading, airborne_heading)
-                if outside_polygon or heading_changed:
-                    takeoff_start = events[idx + 1].timestamp
-                    break
+                if event_has_location(e):
+                    if not point_inside_runway(e.latitude, e.longitude, rwy_polygon):
+                        takeoff_start = events[idx + 1].timestamp
+                        break
+
+                    curr_distance = haversine(
+                        e.latitude, e.longitude,
+                        matched_end.latitude, matched_end.longitude,
+                    )
+                    if curr_distance > min_distance:
+                        takeoff_start = events[idx + 1].timestamp
+                        break
+                    else:
+                        min_distance = curr_distance
         else:
             def headingIsOutOfRange(e: FlightEvent)->bool:
                 return (
